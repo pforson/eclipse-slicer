0902142f9672498c8d0cf822a775ff25c3a0f745
IShowerNaked <rybackej@student.hu-berlin.de>
2017-11-24 18:22:16 +0100

    Refactoring

--- de.hu-berlin.slice.plugin/src/de/hu_berlin/slice/handlers/SampleHandler.java
+++ de.hu-berlin.slice.plugin/src/de/hu_berlin/slice/handlers/SampleHandler.java
@@ -3,12 +3,11 @@ package de.hu_berlin.slice.handlers;
 import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.jface.dialogs.MessageDialog;
 import org.eclipse.ui.IWorkbenchWindow;
 import org.eclipse.ui.handlers.HandlerUtil;
 
 import com.ibm.wala.ipa.callgraph.AnalysisScope;
-
-import org.eclipse.jface.dialogs.MessageDialog;
 
 /**
  * Our sample handler extends AbstractHandler, an IHandler base class.
@@ -20,9 +19,9 @@ public class SampleHandler extends AbstractHandler {
 	@Override
 	public Object execute(ExecutionEvent event) throws ExecutionException {
 		IWorkbenchWindow window = HandlerUtil.getActiveWorkbenchWindowChecked(event);
-		
+
 		AnalysisScope scope = AnalysisScope.createJavaAnalysisScope();
-		
+
 		MessageDialog.openInformation(
 				window.getShell(),
 				"Slice",
