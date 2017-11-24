d28dc368c1c6baf69685133e835c8202abc7f1cd
IShowerNaked <rybackej@student.hu-berlin.de>
2017-11-24 15:06:06 +0100

    Automated copying of Maven dependencies

--- de.hu_berlin.slice/src/de/hu_berlin/slice/handlers/SampleHandler.java
+++ de.hu_berlin.slice/src/de/hu_berlin/slice/handlers/SampleHandler.java
@@ -5,6 +5,9 @@ import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
 import org.eclipse.ui.IWorkbenchWindow;
 import org.eclipse.ui.handlers.HandlerUtil;
+
+import com.ibm.wala.ipa.callgraph.AnalysisScope;
+
 import org.eclipse.jface.dialogs.MessageDialog;
 
 /**
@@ -17,10 +20,13 @@ public class SampleHandler extends AbstractHandler {
 	@Override
 	public Object execute(ExecutionEvent event) throws ExecutionException {
 		IWorkbenchWindow window = HandlerUtil.getActiveWorkbenchWindowChecked(event);
+		
+		AnalysisScope scope = AnalysisScope.createJavaAnalysisScope();
+		
 		MessageDialog.openInformation(
 				window.getShell(),
 				"Slice",
-				"Hello, Eclipse world");
+				"Hello, Eclipse world" + " ~> " + scope.getLanguages());
 		return null;
 	}
 }
